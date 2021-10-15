bf738df1b579c4d2180e4e1e2c6e3d1be0472874
Jonathan <jonathan.trhodes07@gmail.com>
2021-10-15 10:33:31 -0500

    warmup exercise complete

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Locale;
 //work in progress
 public class JavaDrills {
@@ -9,14 +10,31 @@ public class JavaDrills {
         input.replace(substring, last);
         return input;
     }
+    static int[] arrayOne = new int[] {2,4,10,9};
+    static int[] arrayTwo = new int[] {8,5,3,2};
 
+    public static int returnTotalDifference(int[] secondSumArray,int[] sumArray){
+        int sum = 0;
+        int secondSum = 0;
+        for(Integer num: sumArray){
+            sum += num;
+        }
+        for(Integer num: secondSumArray){
+            secondSum += num;
+        }
+        return sum-secondSum;
+    }
 
+//    In `codeup-java-exercises`, in `JavaDrills`:
+//    Create a static method, returnTotalDifference, that takes in two lists of integers and returns the difference between the sum of all integers in the first list minus the sum of all integers in the second list.
+//            returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)); // returns 9
+//    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)); // returns 3
+//    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)); // returns 2
     //    Create a public static method, `flipOuterCase`, that takes in a string and returns the string
 //        with the case reversed for the first and last letters. Assume the input will be three or more characters long.
 //        Example inputs and expected outputs...
     public static void main(String[] args) {
-
-
+        System.out.println(returnTotalDifference(arrayOne, arrayTwo));
         System.out.println(flipOuterCase("cat"));
         System.out.println(flipOuterCase("CaT"));
         System.out.println(flipOuterCase("caT"));
